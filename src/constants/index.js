69f05a96b948237ba5b454821b8bfb9e66e030a9
abhishekrithik <abhishekrithik2017@gmail.com>
2025-08-31 13:23:46 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,299 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Education',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+
+export const myProjects = [
+  {
+  title: 'MERN Blog Platform',
+  desc: 'A full-stack blog application where I share my thoughts on education, fitness, finance, travel, and technology. Built with the MERN stack, it provides a seamless and secure blogging experience.',
+  subdesc:
+    'The platform includes authentication with JWT & Google OAuth, CRUD operations for posts, comments, and users, an admin dashboard, advanced search with MongoDB, and a responsive UI with dark mode.',
+  href: 'https://mern-blog-saiabhi.onrender.com/',
+  texture: '/textures/project/project1.mp4',
+  logo: '/assets/project-logo2.png',
+  logoStyle: {
+    backgroundColor: '#13202F',
+    border: '0.2px solid #17293E',
+    boxShadow: '0px 0px 60px 0px #2F6DB54D',
+  },
+  spotlight: '/assets/spotlight2.png',
+  tags: [
+    {
+      id: 1,
+      name: 'React.js',
+      path: '/assets/react.svg',
+    },
+    {
+      id: 2,
+      name: 'TailwindCSS',
+      path: 'assets/tailwindcss.png',
+    },
+    {
+      id: 3,
+      name: 'Node.js',
+      path: '/assets/nodejs.png',
+    },
+    {
+      id: 4,
+      name: 'MongoDB',
+      path: '/assets/mongodb.png',
+    },
+    {
+      id: 5,
+      name: 'Express.js',
+      path: '/assets/express.png',
+    },
+    {
+      id: 6,
+      name: 'Redux Toolkit',
+      path: '/assets/redux.png',
+    },
+  ],
+},
+  {
+  title: 'MERN Expense Tracker',
+  desc: 'A full-stack expense tracker app built with the MERN stack, enabling users to securely manage their income and expenses with a clean and responsive interface.',
+  subdesc:
+    'This application features JWT-based authentication, interactive dashboard with total balance, income, and expenses, CRUD operations for income/expenses, recent transactions, Excel export, and visual insights with interactive charts. Fully responsive across all devices.',
+  href: 'https://income-and-expense-tracker-sai.vercel.app/',
+  texture: '/textures/project/project2.mp4',
+  logo: '/assets/project-logo1.png',
+  logoStyle: {
+    backgroundColor: '#13202F',
+    border: '0.2px solid #17293E',
+    boxShadow: '0px 0px 60px 0px #2F6DB54D',
+  },
+  spotlight: '/assets/spotlight2.png',
+  tags: [
+    {
+      id: 1,
+      name: 'React.js',
+      path: '/assets/react.svg',
+    },
+    {
+      id: 2,
+      name: 'TailwindCSS',
+      path: 'assets/tailwindcss.png',
+    },
+    {
+      id: 3,
+      name: 'Node.js',
+      path: '/assets/nodejs.png',
+    },
+    {
+      id: 4,
+      name: 'MongoDB',
+      path: '/assets/mongodb.png',
+    },
+    {
+      id: 5,
+      name: 'Express.js',
+      path: '/assets/express.png',
+    },
+    {
+      id: 6,
+      name: 'JWT Auth',
+      path: '/assets/jwt.png',
+    },
+    {
+      id: 7,
+      name: 'Chart.js',
+      path: '/assets/chartjs.png',
+    },
+  ],
+},
+  {
+  title: 'Movie App 🎬',
+  desc: 'A responsive movie discovery platform built with React.js, Appwrite, and TailwindCSS. It allows users to browse trending movies, search titles, and explore content using the TMDB API, all in a sleek and modern interface.',
+  subdesc:
+    'Designed for performance and scalability, the Movie App leverages React hooks, Appwrite backend services, and Vite for fast builds. With features like trending algorithms, real-time search, and responsive UI, it ensures a smooth browsing experience across devices.',
+  href: 'https://movie-app-react25.netlify.app/',
+  texture: '/textures/project/project3.mp4',
+  logo: '/assets/project-logo3.png',
+  logoStyle: {
+    backgroundColor: '#60f5a1',
+    background:
+      'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+    border: '0.2px solid rgba(208, 213, 221, 1)',
+    boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+  },
+  spotlight: '/assets/spotlight3.png',
+  tags: [
+    {
+      id: 1,
+      name: 'React.js',
+      path: '/assets/react.svg',
+    },
+    {
+      id: 2,
+      name: 'TailwindCSS',
+      path: 'assets/tailwindcss.png',
+    },
+    {
+      id: 3,
+      name: 'Appwrite',
+      path: '/assets/appwrite.png',
+    },
+    {
+      id: 4,
+      name: 'TMDB API',
+      path: '/assets/tmdb.png',
+    },
+  ],
+},
+  {
+  title: 'Google Drive Clone- StoreIt📂',
+  desc: 'A powerful storage management and file-sharing platform that allows users to effortlessly upload, organize, and share files. Built with Next.js 15, React 19, and Appwrite, it provides seamless file handling with a clean and responsive interface.',
+  subdesc:
+    'This project showcases advanced features like authentication, file uploads, sharing, and dashboard insights. With support for global search, sorting, and a modern responsive design, the platform ensures efficient file management and collaboration across devices.',
+  href: 'https://saiabhi-store-it.vercel.app/',
+  texture: '/textures/project/project4.mp4',
+  logo: '/assets/project-logo4.png',
+  logoStyle: {
+    backgroundColor: '#60f5a1',
+    background:
+      'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+    border: '0.2px solid rgba(208, 213, 221, 1)',
+    boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+  },
+  spotlight: '/assets/spotlight4.png',
+  tags: [
+    {
+      id: 1,
+      name: 'React 19',
+      path: '/assets/react.svg',
+    },
+    {
+      id: 2,
+      name: 'Next.js 15',
+      path: '/assets/nextjs.png',
+    },
+    {
+      id: 3,
+      name: 'Appwrite',
+      path: '/assets/appwrite.png',
+    },
+    {
+      id: 4,
+      name: 'TailwindCSS',
+      path: '/assets/tailwindcss.png',
+    },
+    {
+      id: 5,
+      name: 'ShadCN',
+      path: '/assets/shadcn.png',
+    },
+    {
+      id: 6,
+      name: 'TypeScript',
+      path: '/assets/typescript.png',
+    },
+  ],
+},
+  {
+  title: 'Subscription Management System API',
+  desc: 'A production-ready backend API that manages real users, subscriptions, and business logic. It features secure JWT authentication, advanced rate limiting, database modeling, and automated workflows for a scalable and secure experience.',
+  subdesc:
+    'Built with Node.js, Express.js, and MongoDB, this API integrates Arcjet for bot protection, Mongoose for modeling, and Upstash for automated reminders—ensuring real-world, production-level backend readiness.',
+  href: 'https://subscription-tracker-sai.onrender.com', 
+  texture: '/textures/project/project5.mp4', 
+  logo: '/assets/project-logo5.png',
+  logoStyle: {
+    backgroundColor: '#111827',
+    border: '0.2px solid #1F2937',
+    boxShadow: '0px 0px 60px 0px #3741514D',
+  },
+  spotlight: '/assets/spotlight5.png',
+  tags: [
+    {
+      id: 1,
+      name: 'Node.js',
+      path: '/assets/nodejs.png',
+    },
+    {
+      id: 2,
+      name: 'Express.js',
+      path: '/assets/express.png',
+    },
+    {
+      id: 3,
+      name: 'MongoDB',
+      path: '/assets/mongodb.png',
+    },
+    {
+      id: 4,
+      name: 'Mongoose',
+      path: '/assets/mongoose.png',
+    },
+    {
+      id: 5,
+      name: 'JWT',
+      path: '/assets/jwt.png',
+    },
+  ],
+},
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: "Zen Class Full Stack Developer Program",
+    pos: 'HCL GUVI',
+    duration: '2024',
+    title: `Gained hands-on expertise in React, Node.js, Express, and MongoDB with real-world deployments. Completed 50+ mini projects and 5+ MERN stack projects and continue to develop MERN stack applications.`,
+    icon: "/assets/guvihcl.png",
+    animation: 'victory',
+  },
+  {
+    id: 2,
+    name: 'Google Project Management: Professional Certificate',
+    pos: 'Web Coursera',
+    duration: 'Apr 2023 – Nov 2023',
+    title: `Built strong foundations in Agile & Scrum methodologies.
+    Gained skills in documentation, stakeholder management, and strategic communication.`,
+    icon: "/assets/certificate.png",
+    animation: 'clapping',
+  },
+  {
+    id: 3,
+    name: 'B.Tech Information Technology',
+    pos: 'Sri Krishna College of Technology, Coimbatore',
+    duration: '2019 – 2023',
+    title: `Final Year Project: Brain Malignancy Detection using Deep Learning on MRI scans. 
+  Achieved 85% accuracy on 1,000+ images with advanced preprocessing, boosting model performance by 30%. 
+  Presented findings at a local tech seminar with 50+ industry professionals.`,
+    icon: '/assets/skct.jpeg',
+    animation: 'salute',
+  },
+];
